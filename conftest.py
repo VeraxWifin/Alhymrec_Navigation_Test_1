b67c9e6c6723210609cc05a3c1b13bb9464dd3f8
Alexander L <echtopy@gmail.com>
2023-04-22 19:07:47 +0300

    conftest.py update.

--- conftest.py
+++ conftest.py
@@ -2,8 +2,6 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.firefox.service import Service
 import allure
 from allure_commons.types import AttachmentType
 
@@ -13,15 +11,7 @@ def browser_chrome():
     options = webdriver.ChromeOptions()
     service = ChromeService(ChromeDriverManager().install())
     browser = webdriver.Chrome(service=service, options=options)
-    # browser.maximize_window()
+    browser.maximize_window()
     yield browser
     allure.attach(browser.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
     browser.quit()
-
-
-# @pytest.fixtrue()
-# def browser_firefox():
-#     service = Service(GeckoDriverManager().install())
-#     browser = webdriver.Friefox(service=service)
-#     yield browser
-#     browser.quit()
